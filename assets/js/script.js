e96b26e7a02062e193b8822a56231ce30cc56110
Craig Roberts <craig.roberts11@outlook.com>
2023-01-26 11:58:06 +1030

    Converted fetch calls to async functions. getMiddayIndex working too

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 
 //http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
 
+// http://openweathermap.org/img/wn/${icon}@2x.png
+
 const apiKey = '7d23a37898f652dad9213e544cd70c75';
 // capture references to HTML
 const cityInput = $('#city-input');
@@ -10,34 +12,72 @@ const currentWeatherSection = $('#current-weather');
 const futureWeatherSection = $('#future-weather');
 
 let weatherData;
+let middayIndex;
 
-function searchWeather(event) {
+async function searchWeather(event) {
   event.preventDefault();
 
+  // if no data entered, do nothing
   if (!$(cityInput).val()) return;
+
   const location = $(cityInput).val();
-  let lat, long;
 
   const cityUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=5&appid=${apiKey}`;
-  fetch(cityUrl)
+  const [lat, long] = await getCityCoords(cityUrl);
+
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
+  await getWeatherDetails(weatherUrl);
+}
+
+async function getCityCoords(cityUrl) {
+  return fetch(cityUrl)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      lat = data[0].lat;
-      long = data[0].lon;
-      const weatherUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${apiKey}`;
-
-      return fetch(weatherUrl)
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          weatherData = data;
-          console.log(data);
-        });
+      return [data[0].lat, data[0].lon];
     })
-    .catch((msg) => {
-      console.log(msg);
+    .catch((reason) => {
+      console.log(reason);
     });
+}
+
+async function getWeatherDetails(weatherUrl) {
+  return fetch(weatherUrl)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      weatherData = data;
+      console.log(weatherData);
+      displayCurrentWeather(location);
+    })
+    .catch((reason) => {
+      console.log(reason);
+    });
+}
+
+// Finds the first instance of midday in the returned dataset
+function determineMiddayIndex() {
+  const day = weatherData.list.find((day, i) => {
+    return day['dt_txt'].endsWith('12:00:00');
+  });
+
+  const index = weatherData.list.indexOf(day);
+  return index;
+}
+
+function displayCurrentWeather(location) {
+  if (weatherData === null) return;
+  middayIndex = determineMiddayIndex();
+
+  const card = $('<div></div>');
+  const cardHeader = $('<p></p>');
+  const weatherImgSpan = $('<span></span');
+  const weatherImg = $('<img></img>');
+  const temperatureText = $('<p></p>');
+  const windText = $('<p></p>');
+  const humidityText = $('<p></p>');
+
+  $(cardHeader).text(`${location.toUpperCase()} (${data[middayIndex]})`);
 }
